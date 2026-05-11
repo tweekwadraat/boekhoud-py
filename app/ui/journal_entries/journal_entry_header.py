24ba1e49efb3875672e2e5f1ff3865d86a1ba18f
tweekwadraat <244897919+tweekwadraat@users.noreply.github.com>
2026-05-11 10:38:47 +0200

    Make journal entry header fields editable

--- app/ui/journal_entries/journal_entry_header.py
+++ app/ui/journal_entries/journal_entry_header.py
@@ -1,7 +1,7 @@
 from textual.widget import Widget
 from textual.app import ComposeResult
 from textual.containers import HorizontalGroup
-from textual.widgets import Static
+from textual.widgets import Static, Input, Label
 
 
 class JournalEntryHeader(Widget):
@@ -15,19 +15,39 @@ class JournalEntryHeader(Widget):
     JournalEntryHeader {
         height: auto;
     }
+    
     JournalEntryHeader HorizontalGroup Static.balance {
         width: 1fr;
         text-align: right;
     }
+    
     JournalEntryHeader HorizontalGroup Static {
         width: auto;
+    }
+
+    JournalEntryHeader Input {
+        border: none;
+        height: 1;
+        width: 30;
+    }
+
+    JournalEntryHeader Input:focus {
+        border: none;
+        height: 1;
     }
     """
 
     def compose(self) -> ComposeResult:
         with HorizontalGroup():
-            yield Static('Dagboek      : VER1')
+            yield Label('Dagboek      :')
+            yield Input(value='VER1', id='diary')
             yield Static('Saldo: 0.00', classes='balance')
-        yield Static('Boekstuknr   : 2026-0042')
-        yield Static('Datum        : 15-04-2026')
-        yield Static('Omschrijving : Factuur april diensten')
+        with HorizontalGroup():
+            yield Label('Boekstuknr   :')
+            yield Input(value='2026-0042', id='entry_number')
+        with HorizontalGroup():
+            yield Label('Datum        :')
+            yield Input(value='15-04-2026', id='date')
+        with HorizontalGroup():
+            yield Label('Omschrijving :')
+            yield Input(value='Factuur april diensten', id='description')
